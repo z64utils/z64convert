f79df110fc4eb10e361b167bc5cd406832c2995a
z64me <45853441+z64me@users.noreply.github.com>
2022-02-02 19:12:43 -0500

    extra `-lm` for linker errors, thanks @Dragorn421!

--- common.sh
+++ common.sh
@@ -1,1 +1,1 @@
-echo "-DGBI_PREFIX=F3DEX2 -DVFILE_VISIBILITY=static -DNDEBUG -Wno-unused-function -Wno-unused-variable -Wall -flto -lm -Os -s -flto -Igfxasm/src -Iwowlib -DWOW_OVERLOAD_FILE src/*.c gfxasm/src/*.c -Isrc "
+echo "-DGBI_PREFIX=F3DEX2 -DVFILE_VISIBILITY=static -DNDEBUG -Wno-unused-function -Wno-unused-variable -Wall -flto -lm -Os -s -flto -Igfxasm/src -Iwowlib -DWOW_OVERLOAD_FILE src/*.c gfxasm/src/*.c -Isrc -lm "
