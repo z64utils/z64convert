z64utils/z64convert

commit ad9d0340ccec2007e88d650266953446d52802ef

feature: single file input -> multi-file output
z64convert is now capable of accepting OBJEX2 input files containing multiple game rooms defined using special tags. To save space, it determines which assets (textures, palettes, and materials) are shared among rooms, and it optimizes them into a common data bank. All rooms, including the data bank, are exported as separate files. This commit satisfies the test condition `test/CommonSimpleSingle`.

This feature isn't limited to game worlds, but those will be the most common use-case. In the future, we may extend it to support external animation banks for skeletal meshes. Should we wish to, test files must be created first.

src/texture.c
@@ -901,6 +901,8 @@ void *texture_writeTexture(VFILE *bin, struct objex_texture *tex)
 	
 	/* get texture offset */
 	udata->fileOfs = vftell(bin);
+	if (tex->pointer == 0)
+		tex->pointer = udata->fileOfs + getBase(tex->objex);
 //	fprintf(docs, "texture '%s' %08X (%08X)\n", tex->name, (int)udata->fileOfs, (int)udata->fileSz);
 	// fprintf(docs, DOCS_DEF "TEX_" DOCS_SPACE "  0x%08X\n"
 	// 	, Canitize(pathTail(tex->name), 1)
@@ -909,7 +911,7 @@ void *texture_writeTexture(VFILE *bin, struct objex_texture *tex)
 	document_assign(
 		pathTail(tex->name),
 		NULL,
-		(int)udata->fileOfs + getBase(tex->objex),
+		tex->pointer,
 		T_TEX
 	);
 	
@@ -942,8 +944,7 @@ void *texture_writeTexture(VFILE *bin, struct objex_texture *tex)
 			document_assign(
 				buffer,
 				NULL,
-				(int)(udata->fileOfs
-				+ getBase(tex->objex)
+				(int)(tex->pointer
 				+ i * (udata->fileSz / udata->virtDiv)),
 				T_TEX
 			);
@@ -993,13 +994,15 @@ void *texture_writePalette(VFILE *bin, struct objex_palette *pal)
 	
 	/* write palette into file */
 	pal->fileOfs = vftell(bin);
+	if (pal->pointer == 0)
+		pal->pointer = pal->fileOfs + getBase(pal->objex);
 	color8 = pal->colors;
 	//fprintf(docs, "palette %08X (sz %d)\n", (int)pal->fileOfs, pal->colorsSize);
 	if (printPalettes)
 		document_assign(
 			int2str(pal->index),
 			NULL,
-			(int)pal->fileOfs + getBase(pal->objex),
+			pal->pointer,
 			T_PAL
 		);
 		// fprintf(docs, DOCS_DEF "PAL_" DOCS_SPACE "  0x%08X\n"

src/objex.c
@@ -2312,6 +2312,7 @@ struct objex *objex_load(
 	struct objex_g *g = 0;
 	struct objex_material *mtl = 0;
 	struct objex_f *fPrev = 0;
+	struct objex_file *file = 0;
 	error_reason = ERR_NONE;
 	int weightless = 0;
 	int weighted = 0;
@@ -2353,6 +2354,8 @@ struct objex *objex_load(
 			objex->vtNum++;
 		else if (streq24(ss, "vc "))
 			objex->vcNum++;
+		else if (streq32(ss, "file "))
+			objex->fileNum++;
 		else if (streq32(ss, "version ") && (hasVersion = 1)
 			&& sscanf(ss, "version %d.%d", &version, &versionMajor) != 2
 		)
@@ -2414,18 +2417,20 @@ struct objex *objex_load(
 	objex->vnNum += !objex->vnNum;
 	objex->vtNum += !objex->vtNum;
 	objex->vcNum += !objex->vcNum;
+	objex->fileNum += !objex->fileNum;
 	
-	/* allocate v, vn, vt */
+	/* allocate v, vn, vt, vc, file */
 	if (
 		!(objex->v = calloc(objex->vNum, sizeof(*objex->v)))
 		|| !(objex->vn = calloc(objex->vnNum, sizeof(*objex->vn)))
 		|| !(objex->vt = calloc(objex->vtNum, sizeof(*objex->vt)))
 		|| !(objex->vc = calloc(objex->vcNum, sizeof(*objex->vc)))
+		|| !(file = objex->file = calloc(objex->fileNum, sizeof(*objex->file)))
 	)
 		fail(ERR_NOMEM);
 	
 	/* parse raw obj */
-	objex->vNum = objex->vnNum = objex->vtNum = objex->vcNum = 0;
+	objex->fileNum = objex->vNum = objex->vnNum = objex->vtNum = objex->vcNum = 0;
 	lineNum = 1;
 	for (const char *ss = raw; ss; (ss = nextlineNum(ss, &lineNum)))
 	{
@@ -2543,6 +2548,36 @@ struct objex *objex_load(
 			vc->g = g;
 			vc->b = b;
 			vc->a = a;
+		}
+		else if (streq32(ss, "file "))
+		{
+			const char *tmp;
+			const char *comm;
+			int ssLen = strcspn(ss, "\r\n");
+			file = objex->file + objex->fileNum;
+			objex->fileNum++;
+			
+			/* fetch name */
+			if (!(tmp = nexttok_linerem(ss, 1)))
+				return errmsg(
+					"could not fetch file name from '%.*s'"
+					, ssLen, ss
+				);
+			if (!(file->name = strdup(tmp)))
+				fail(ERR_NOMEM);
+			
+			/* address */
+			if ((tmp = nexttok_linerem(ss, 2)) && !sscanf(tmp, "%x", &file->baseOfs))
+				return errmsg(
+					"could not fetch hexadecimal address from '%.*s'"
+					, ssLen, ss
+				);
+			//fprintf(stderr, "baseOfs = %08x\n", file->baseOfs);
+			
+			/* common */
+			/* TODO: fail if more than one file marked common? */
+			if ((comm = strstr(ss, "common")) && comm < ss + ssLen)
+				file->isCommon = 1;
 		}
 		else if (streq32(ss, "useskel "))
 		{
@@ -2692,6 +2727,7 @@ struct objex *objex_load(
 				fail0(0);
 			
 			/* zero-initialize */
+			g->file = file;
 			weightless = 0;
 			weighted = 0;
 			fPrev = 0;
@@ -2954,6 +2990,11 @@ struct objex *objex_load(
 			mtl = material_find(objex, name);
 			if (!mtl)
 				fail("could not find material '%s'", name);
+			
+			/* same material spans multiple files */
+			if (mtl->file && mtl->file != file)
+				mtl->isMultiFile = 1;
+			mtl->file = file;
 		}
 		else if (flags & OBJEX_UNKNOWN_DIRECTIVES)
 			fail("unknown directive '%.*s'", strcspn(ss, " \n"), ss);
@@ -3008,6 +3049,169 @@ void objex_localize(struct objex *objex)
 	struct objex_v *v;
 	for (v = objex->v; v - objex->v < objex->vNum; ++v)
 		*v = vert_xform(v);
+}
+
+// TODO rework (omit?) the whole udata system
+extern void *zobj_initObjex(struct objex *obj, const unsigned int baseOfs, FILE *docs);
+
+static int qsortfunc_fileCommonFirst(const void *a_, const void *b_)
+{
+	const struct objex_file *a = a_, *b = b_;
+	
+	if (a->isCommon)
+		return -1;
+	
+	return 1;
+}
+
+void *objex_divide(struct objex *objex, FILE *docs)
+{
+	struct objex_file *common = 0;
+	
+	if (objex->fileNum <= 1)
+		return success;
+	
+	/* divide group list across files */
+	for (int i = 0; i < objex->fileNum; ++i)
+	{
+		struct objex_file *file = objex->file + i;
+		struct objex_g **prev = &objex->g;
+		struct objex_g *next = 0;
+		struct objex *newObj;
+		
+		/* allocate objex structure */
+		if (!(newObj = file->objex = calloc(1, sizeof(*file->objex))))
+			return errmsg(ERR_NOMEM);
+		
+		zobj_initObjex(newObj, file->baseOfs ? file->baseOfs : objex->baseOfs, docs);
+		
+		if (file->isCommon)
+		{
+			if (common)
+				return errmsg(
+					"multiple common files specified: "
+					"'%s' and '%s'"
+					, common->name, file->name
+				);
+			common = file;
+		}
+		
+		for (struct objex_g *g = objex->g; g; g = next)
+		{
+			next = g->next;
+			
+			if (g->file == file)
+			{
+				g->objex = newObj;
+				
+				/* unlink from old list */
+				if (next)
+				{
+					*prev = next;
+					prev = &next->next;
+				}
+				
+				/* link into new list */
+				g->next = file->head;
+				file->head = g;
+				newObj->gNum += 1;
+				continue;
+			}
+			
+			prev = &g->next;
+		}
+		
+		newObj->g = file->head;
+	}
+	objex->gNum = 0;
+	objex->g = 0;
+	
+	/* XXX could add logic if no common file, but then the data gets
+	 *     duplicated, defeating the purpose of storing multiple files
+	 *     in a single objex (aka such a feature has no real use-case)
+	 */
+	if (!common)
+		return errmsg("no common file specified");
+	
+	/* materials */
+	{
+		struct objex_material *next = 0;
+		
+		for (struct objex_material *m = objex->mtl; m; m = next)
+		{
+			struct objex_file *into = (m->isMultiFile || !m->file) ? common : m->file;
+			struct objex *dst = into->objex;
+			
+			next = m->next;
+			
+			if (m->tex0)
+			{
+				/* same texture spans multiple files */
+				if (m->tex0->file && m->tex0->file != into)
+					m->tex0->isMultiFile = 1;
+				m->tex0->file = into;
+			}
+			
+			if (m->tex1)
+			{
+				/* same texture spans multiple files */
+				if (m->tex1->file && m->tex1->file != into)
+					m->tex1->isMultiFile = 1;
+				m->tex1->file = into;
+			}
+			
+			m->objex = dst;
+			m->next = dst->mtl;
+			dst->mtl = m;
+			dst->mtlNum += 1;
+		}
+		
+		objex->mtl = 0;
+		objex->mtlNum = 0;
+	}
+	
+	/* textures and palettes */
+	{
+		struct objex_texture *next = 0;
+		
+		for (struct objex_texture *tex = objex->tex; tex; tex = next)
+		{
+			struct objex_file *into = (tex->isMultiFile || !tex->file) ? common : tex->file;
+			struct objex *dst = into->objex;
+			
+			next = tex->next;
+			
+			tex->objex = dst;
+			tex->next = dst->tex;
+			dst->tex = tex;
+			
+			if (tex->palette)
+			{
+				tex->palette->objex = dst;
+				tex->palette->next = dst->pal;
+				dst->pal = tex->palette;
+			}
+		}
+		
+		objex->pal = 0;
+		objex->tex = 0;
+	}
+	
+	#if 0
+	for (int i = 0; i < objex->fileNum; ++i)
+	{
+		struct objex_file *file = objex->file + i;
+		struct objex *objex = file->objex;
+		
+		fprintf(stderr, "%p %s\n", objex->g, objex->g->name);
+	}
+	fprintf(stderr, "main: %p\n", objex->g);
+	return errmsg("hey");
+	#endif
+	
+	qsort(objex->file, objex->fileNum, sizeof(*objex->file), qsortfunc_fileCommonFirst);
+	
+	return success;
 }
 
 const char *objex_errmsg(void)
@@ -3501,6 +3705,18 @@ void objex_free(struct objex *objex, void free(void *))
 	if (objex->vn) free(objex->vn);
 	if (objex->vt) free(objex->vt);
 	if (objex->vc) free(objex->vc);
+	if (objex->file)
+	{
+		for (int i = 0; i < objex->fileNum; ++i)
+		{
+			struct objex_file *file = objex->file + i;
+			if (file->name)
+				free(file->name);
+			if (file->objex)
+				objex_free(file->objex, free);
+		}
+		free(objex->file);
+	}
 	
 	/* free softinfo */
 	free_if(objex->softinfo.animation_framerate);

src/objex.h
@@ -67,6 +67,7 @@ enum objex_gbivar /* NOTE: do not change order! */
 
 struct objex;
 struct objex_g;
+struct objex_file;
 
 typedef void (*objex_udata_free)(void *);
 
@@ -89,6 +90,7 @@ extern struct objex *objex_load(
 	, enum objex_flag flags
 );
 extern const char *objex_errmsg(void);
+extern void *objex_divide(struct objex *objex, FILE *docs);
 extern void objex_localize(struct objex *objex);
 extern void objex_resolve_common(struct objex *dst, struct objex *needle, struct objex *haystack);
 extern void objex_resolve_common_array(struct objex *dst, struct objex *src[], int srcNum);
@@ -133,6 +135,7 @@ struct objex_palette
 	int colorsNum;
 	int colorsSize;
 	unsigned int fileOfs;
+	unsigned int pointer; /* pointer override */
 	int isUsed;
 	int index; /* Nth item initialized, starting at 0 (order created) */
 };
@@ -145,6 +148,7 @@ struct objex_texture
 	struct objex_palette *palette;
 	struct objex_texture *commonRef;
 	struct objex_texture *copyOf;
+	struct objex_file *file;
 	struct objex *objex;
 	OBJ_NAMECONST char *filename;  /* filename used for loading texture */
 	char *format;    /* custom format string */
@@ -153,6 +157,7 @@ struct objex_texture
 	OBJ_NAMECONST char *instead;   /* file to use pixel data from instead */
 	void *pix;       /* rgba pixel data */
 	int isUsed;
+	int isMultiFile;
 	int alwaysUsed;
 	int alwaysUnused;
 	int priority;
@@ -175,6 +180,7 @@ struct objex_material
 	struct objex_material *next;
 	struct objex_texture *tex0;
 	struct objex_texture *tex1;
+	struct objex_file *file;
 	struct objex *objex;
 	OBJ_NAMECONST char *name;
 	char *gbi;
@@ -185,6 +191,7 @@ struct objex_material
 	int isUsed;
 	int isStandalone;
 	int isEmpty;
+	int isMultiFile;
 	int alwaysUsed;
 	int hasWritten;
 	int priority;
@@ -310,6 +317,7 @@ struct objex_g
 	struct objex_f *f;
 	struct objex *objex;
 	struct objex_bone *bone;
+	struct objex_file *file;
 	struct objex_skeleton *skeleton;
 	OBJ_NAMECONST char *name;
 	char *attrib;
@@ -329,6 +337,16 @@ struct objex_g
 	int priority;
 };
 
+/* file */
+struct objex_file
+{
+	OBJ_NAMECONST char *name;
+	struct objex_g *head;
+	struct objex *objex;
+	unsigned int baseOfs;
+	int isCommon;
+};
+
 /* a parsed objex file */
 struct objex
 {
@@ -343,18 +361,21 @@ struct objex
 	struct objex_texture *tex;
 	struct objex_palette *pal;
 	struct objex_g *g;
+	struct objex_file *file;
 	char *mtllibDir; /* directory of mtllib (used for texture fopen) */
 	int vNum;
 	int vnNum;
 	int vtNum;
 	int vcNum;
 	int gNum;
+	int fileNum;
 	int mtlNum;
 	struct
 	{
 		char *animation_framerate;
 	} softinfo;
 	struct objexString *string;
+	unsigned int baseOfs;
 };
 #endif /* OBJEX_H_INCLUDED */
 

src/z64convert.c
@@ -264,6 +264,52 @@ static const char *model_commit(struct model *model)
 	if (!model)
 		fail("model_commit error: model not loaded?");
 	
+	if (model->obj->fileNum > 1)
+	{
+		const char *outFn = model->outFn;
+		struct objex *tmp = model->obj;
+		
+		for (int i = 0; i < tmp->fileNum; ++i)
+		{
+			struct objex_file *file = tmp->file + i;
+			struct objex *obj = file->objex;
+			char buf[1024];
+			int doBreak = 0;
+			
+			obj->v = tmp->v;
+			obj->vt = tmp->vt;
+			obj->vn = tmp->vn;
+			obj->vc = tmp->vc;
+			obj->vNum = tmp->vNum;
+			obj->vtNum = tmp->vtNum;
+			obj->vnNum = tmp->vnNum;
+			obj->vcNum = tmp->vcNum;
+			
+			model->obj = obj;
+			snprintf(buf, sizeof(buf), "%s_%s", outFn, file->name);
+			model->outFn = buf;
+			model->baseOfs = file->baseOfs;
+			if (model_commit(model))
+				doBreak = 1;
+			
+			obj->v = 0;
+			obj->vt = 0;
+			obj->vn = 0;
+			obj->vc = 0;
+			obj->vNum = 0;
+			obj->vtNum = 0;
+			obj->vnNum = 0;
+			obj->vcNum = 0;
+			
+			if (doBreak)
+				break;
+		}
+		
+		model->obj = tmp;
+		model->outFn = outFn;
+		return sgRval;
+	}
+	
 	const char *outMode = "wb+";
 	const char *in = model->inFn;
 	const char *out = model->outFn;
@@ -289,11 +335,6 @@ static const char *model_commit(struct model *model)
 	/* write textures and palettes to out file */
 	if (!texture_writeTextures(zobj, obj)
 	   || !texture_writePalettes(zobj, obj)
-	)
-		fail(texture_errmsg());
-	
-	/* process materials */
-	if (!texture_procMaterials(obj)
 	)
 		fail(texture_errmsg());
 	
@@ -688,6 +729,10 @@ gsSPClearGeometryMode(G_TEXTURE_GEN | G_TEXTURE_GEN_LINEAR),\n\
 		}
 	}
 	
+	/* process materials */
+	if (!texture_procMaterials(obj))
+		fail(texture_errmsg());
+	
 	/* collider C output */
 	/* TODO @rankaisija please adapt this to use doc.c or doc.h in some way */
 	for (struct objex_g *g = obj->g; g; g = g->next)
@@ -1031,6 +1076,10 @@ gsSPClearGeometryMode(G_TEXTURE_GEN | G_TEXTURE_GEN_LINEAR),\n\
 		}
 	}
 	
+	/* if applicable, internally divide the objex into more objex's */
+	if (!objex_divide(obj, docs))
+		fail(objex_errmsg());
+	
 L_cleanup:
 	/* cleanup */
 	m->obj = obj;
@@ -1121,7 +1170,7 @@ const char *z64convert(
 		return "'only' and 'except' cannot be used simultaneously";
 	
 	/* ad hoc multi-file test */
-	#if 1
+	#if 0
 	{
 		struct model *quicktest(const char *in, const char *out)
 		{

src/zobj.c
@@ -330,6 +330,7 @@ void *zobj_initObjex(struct objex *obj, const unsigned int baseOfs, FILE *docs)
 	udata->free = objexUdataFree;
 	udata->baseOfs = baseOfs;
 	udata->docs = docs;
+	obj->baseOfs = baseOfs;
 	
 	return success;
 }

test/CommonSimpleSingle.sh
@@ -1,0 +1,3 @@
+mkdir -p test/bin/CommonSimpleSingle/
+
+valgrind --leak-check=full bin/z64convert-debug --in test/CommonSimpleSingle/complete_room.objex --out test/bin/CommonSimpleSingle/output --print-palettes --header -

test/CommonSimpleSingle/complete_room.objex
@@ -7,7 +7,7 @@ softinfo animation_framerate 24
 mtllib complete_room.mtlex
 skellib complete_room.skel
 animlib complete_room.anim
-file room_1 0x03000000
+file "room_1" 0x03000000
 g "Room1"
 v 7.310000 -0.030000 -0.720839
 v 7.310000 -0.030000 -15.340840
@@ -106,7 +106,7 @@ f 1/41/6 9/38/6 2/42/6
 f 13/43/6 10/44/6 9/45/6
 f 3/37/6 2/42/6 9/38/6
 f 13/43/6 14/46/6 10/44/6
-file room_0 0x03000000
+file "room_0" 0x03000000
 g "Room0"
 v 7.310000 -0.030000 0.724159
 v 7.310000 -0.030000 15.344159
@@ -205,7 +205,7 @@ f 17/87/12 18/88/12 25/83/12
 f 26/89/12 29/90/12 25/91/12
 f 25/83/12 18/88/12 19/84/12
 f 26/89/12 30/92/12 29/90/12
-file scene 0x02000000 common
+file "scene" 0x02000000 common
 g "collision.Collision"
 v 7.310000 -0.030000 0.724159
 v 7.310000 -0.030000 15.344159

commit 29c1404a7595c83e82762fd8ec4c4db6303019b6

bugfix: doc writer now outputs correct texture pointers
Multi-file support commit ad9d034 broke this part of the doc writer. Textures intended to be referenced using RAM segments were having their virtual addresses written instead of their actual addresses.

src/texture.c
@@ -913,7 +913,7 @@ void *texture_writeTexture(VFILE *bin, struct objex_texture *tex)
 	document_assign(
 		pathTail(tex->name),
 		NULL,
-		tex->pointer,
+		udata->fileOfs + getBase(tex->objex),
 		T_TEX
 	);
 	
@@ -946,7 +946,7 @@ void *texture_writeTexture(VFILE *bin, struct objex_texture *tex)
 			document_assign(
 				buffer,
 				NULL,
-				(int)(tex->pointer
+				(int)(udata->fileOfs + getBase(tex->objex)
 				+ i * (udata->fileSz / udata->virtDiv)),
 				T_TEX
 			);
@@ -1004,7 +1004,7 @@ void *texture_writePalette(VFILE *bin, struct objex_palette *pal)
 		document_assign(
 			int2str(pal->index),
 			NULL,
-			pal->pointer,
+			pal->fileOfs + getBase(pal->objex),
 			T_PAL
 		);
 		// fprintf(docs, DOCS_DEF "PAL_" DOCS_SPACE "  0x%08X\n"